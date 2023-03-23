8385fd8a9197f07ad4cca8911378c3cabff14296
RedRain0o0 <redraino.o.o.o.o@gmail.com>
2023-03-22 23:24:15 -0400

    Initial commit

---
+++ src/main/java/com/redrain0o0/SimpleSorceryClient.java
@@ -0,0 +1,10 @@
+package com.redrain0o0;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleSorceryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/redrain0o0/SimpleSorceryDataGenerator.java
@@ -0,0 +1,11 @@
+package com.redrain0o0;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SimpleSorceryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
